7eeffd6b628756e555747e2610e9e0974656f597
Chesss92 <velizcris63@gmail.com>
2026-07-15 15:04:49 -0500

    Usuarios, Logins y privilegios Pt. 2

--- Usuarios_role_privilegios.sql
+++ Usuarios_role_privilegios.sql
@@ -3,11 +3,15 @@
 USE [master]
 GO
 
-CREATE LOGIN usuario_admin WITH PASSWORD = 'Admin2026';
+CREATE LOGIN login_admin WITH PASSWORD = 'Admin2026';
 GO
-CREATE LOGIN usuario_gerente WITH PASSWORD = 'Gerente2026';
+CREATE LOGIN login_gerente WITH PASSWORD = 'Gerente2026';
 GO
-CREATE LOGIN usuario_cajero WITH PASSWORD = 'Cajero2026';
+CREATE LOGIN login_cajero WITH PASSWORD = 'Cajero2026';
+GO
+CREATE LOGIN login_vendedor WITH PASSWORD = 'Vendedor2026';
+GO
+CREATE LOGIN login_auditor WITH PASSWORD = 'Auditor2026';
 GO
 
 --2) Crear usuario dentro de la base de datos 
@@ -15,14 +19,24 @@ GO
 USE [calzado_db]
 GO
 
-CREATE USER usuario_admin FOR LOGIN usuario_admin;
-CREATE USER usuario_gerente FOR LOGIN usuario_gerente;
-CREATE USER usuario_cajero FOR LOGIN usuario_cajero;
+CREATE USER usuario_admin FOR LOGIN login_admin;
+CREATE USER usuario_gerente FOR LOGIN login_gerente;
+CREATE USER usuario_cajero FOR LOGIN login_cajero;
+CREATE USER usuario_vendedor FOR LOGIN login_vendedor;
+CREATE USER usuario_auditor FOR LOGIN login_auditor;
+
 GO
 
---3) Administrador 
+--4) Creacion de Roles
 
 CREATE ROLE rol_administrador;
+CREATE ROLE rol_gerente;
+CREATE ROLE rol_cajero;
+CREATE ROLE rol_vendedor;
+
+
+--5) Administrador 
+
 
 GRANT SELECT TO rol_administrador;
 GRANT INSERT TO rol_administrador;
@@ -38,9 +52,8 @@ GRANT ALTER TO rol_administrador;
 ALTER ROLE rol_administrador ADD MEMBER usuario_admin;
 
 
---4) Gerente
+--6) Gerente
 
-CREATE ROLE rol_gerente;
 
 GRANT SELECT ON dbo.ventas TO rol_gerente;
 GRANT SELECT ON dbo.clientes TO rol_gerente;
@@ -50,9 +63,8 @@ GRANT SELECT ON dbo.productos TO rol_gerente;
 ALTER ROLE rol_gerente ADD MEMBER usuario_gerente;
 
 
---5) Cajero
+--7) Cajero
 
-CREATE ROLE rol_cajero;
 
 GRANT SELECT ON dbo.clientes TO rol_cajero;
 GRANT SELECT ON dbo.productos TO rol_cajero;
@@ -63,3 +75,17 @@ GRANT INSERT ON dbo.detalle_venta TO rol_cajero;
 ALTER ROLE rol_cajero ADD MEMBER usuario_cajero;
 
 
+-- 8) Vendedor
+
+GRANT SELECT ON dbo.clientes TO rol_vendedor;
+GRANT SELECT ON dbo.productos TO rol_vendedor;
+GRANT SELECT ON dbo.ventas TO rol_vendedor;
+
+ALTER ROLE rol_vendedor ADD MEMBER usuario_vendedor;
+
+-- 9) Auditor 
+
+GRANT SELECT ON historial_actividades TO rol_auditor;
+DENY INSERT, UPDATE, DELETE ON historial_actividades TO rol_auditor;
+
+ALTER ROLE rol_auditor ADD MEMBER usuario_auditor;
