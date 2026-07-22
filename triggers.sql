126e458e79ccad090b0c6c05557f2a15421fb9c9
/home/chesss <velizcris63@gmail.com>
2026-07-22 00:23:57 -0500

    Triggers Pt.2

--- triggers.sql
+++ triggers.sql
@@ -96,3 +96,63 @@ BEGIN
     WHERE i.id_sucursal = v.id_sucursal;
 END;
 GO
+
+--5) Incrementar stock por devolucion
+
+CREATE TRIGGER trg_IncrementarStockPorDevolucion
+ON devoluciones
+AFTER INSERT
+AS BEGIN
+    UPDATE i
+    SET i.stock = i.stock + ins.cantidad
+    FROM inventario i
+    INNER JOIN inserted ins ON i.id_producto = ins.id_producto;
+END;
+GO
+
+--6) Control stock negativo
+
+CREATE TRIGGER trg_ControlStockNegativo
+ON inventario
+AFTER INSERT, UPDATE
+AS BEGIN
+    IF EXISTS (SELECT 1 FROM inserted WHERE stock < 0)
+    BEGIN
+        RAISERROR('Error: El stock de un producto no puede ser menor a cero', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
+GO
+
+-- 7) Registro de cambio de precio
+
+CREATE TRIGGER trg_RegistroCambioPrecio
+ON productos
+AFTER UPDATE
+AS BEGIN
+    IF UPDATE(precio_unitario)
+    BEGIN
+        INSERT INTO auditoria (accion, usuario, fecha_accion, tabla_afectada)
+        SELECT 
+            'CAMBIO PRECIO: Anterior $' + CAST(d.precio_unitario AS VARCHAR) + ' / Nuevo $' + CAST(i.precio_unitario AS VARCHAR),
+            SYSTEM_USER,
+            GETDATE(),
+            'productos'
+        FROM inserted i
+        JOIN deleted d ON i.id_producto = d.id_producto;
+    END
+END;
+GO
+
+-- 8) Registro de aacceso
+
+CREATE TRIGGER trg_RegistroAcceso
+ON auditoria
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO auditoria (accion, usuario, fecha_accion, tabla_afectada)
+    VALUES ('Acceso al sistema - Hora: ' + CAST(CAST(GETDATE() AS TIME) AS VARCHAR(8)), SYSTEM_USER, GETDATE(), 'auditoria'
+    );
+END;
+GO
